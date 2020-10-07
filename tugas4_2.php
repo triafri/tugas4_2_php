0b8843bd1fcd5ee5c88c0bb19747b81f43130260
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-10-07 12:30:14 +0700

    Add files via upload

---
+++ tugas4_2.php
@@ -0,0 +1,11 @@
+<?php
+	$saluran = 523;
+	$liter = 8891;
+	$pakai = $liter/$saluran;
+
+
+	echo "Jumlah Saluran = $saluran <br>";
+	echo "Jumlah Terpakai = $liter <br>";
+	echo "Rata-rata Pemakaian = .... ? <br>";
+	echo "Jawaban : $pakai liter/keluarga";
+?>
